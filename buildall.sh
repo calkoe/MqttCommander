f795a6264bd00dd9251e72e8ed99ce46e0ce7258
Calvin Köcher <calvin.koecher@outlook.de>
2021-10-25 13:44:59 +0200

    TOKEN

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
-env GOOS=darwin GOARCH=amd64 go build -o MqttCommander_darwin_amd64 MqttCommander
+env GOOS=darwin GOARCH=amd64 go build -o MqttCommander_darwin_amd64 MqttCommander && ./MqttCommander
 env GOOS=linux GOARCH=amd64 go build -o MqttCommander_linux_amd64 MqttCommander
 env GOOS=linux GOARCH=arm GOARM=5 go build -o MqttCommander_linux_armv5 MqttCommander
 env GOOS=windows GOARCH=amd64 go build -o MqttCommander_windows_amd64.exe MqttCommander
